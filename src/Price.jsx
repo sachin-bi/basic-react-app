049de53390691018e9aa4cab3bff6ba2faf8dd11
Sachinandan <sachinandanp5@gmail.com>
2024-07-09 23:49:24 +0530

    card compents created

--- src/Price.jsx
+++ src/Price.jsx
@@ -1,13 +1,25 @@
 export default function Price({ oldprice, newprice }) {
-    let oldStyles={
-        textDecorationLine:"line-through",
-    };
+  let oldStyles = {
+    textDecorationLine: "line-through",
+  };
+
+  let newStyles = {
+    fontWeight: "bold",
+  };
+
+  let styles = {
+    backgroundColor: "#e0c367",
+    height: "29px",
+    width:"200px",
+    borderBottomLeftRadius: "14px",
+    borderBottomRightRadius: "14px",
+  };
 
   return (
-    <div>
+    <div style={styles}>
       <span style={oldStyles}>{oldprice}</span>
       &nbsp; &nbsp; &nbsp;
-      <span>{newprice}</span>
+      <span style={newStyles}>{newprice}</span>
     </div>
   );
 }
--- src/Product.jsx
+++ src/Product.jsx
@@ -1,15 +1,21 @@
 import "./Product.css";
 import Price from "./Price";
 
-function Product({title,idx}) {
-  let oldprices = ["1599","349","45999","26599"];
-  let newprices = ["1109","249","35899","20999"];
-  let description = ["Brand new design","5000 DPI","Best Specification", "Experience Emersing Sound"];
+function Product({ title, idx }) {
+  let oldprices = ["1599", "349", "45999", "26599"];
+  let newprices = ["1109", "249", "35899", "20999"];
+  let description = [
+    ["Brand new design", "5000 DPI"],
+    ["5000 DPI", "Best Specification"],
+    ["Best Specification","5000 DPI"],
+    ["Experience Emersing ","Enjoy Creating Sound"]
+  ];
   return (
     <div className="Product">
       <h4>{title}</h4>
-      <p>{description[idx]}</p>
-      <Price oldprice={oldprices[idx]} newprice={newprices[idx]}  />
+      <p>{description[idx][0]}</p>
+      <p>{description[idx][1]}</p>
+      <Price oldprice={oldprices[idx]} newprice={newprices[idx]} />
     </div>
   );
 }
